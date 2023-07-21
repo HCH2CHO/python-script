dbfbfb08396acefd7dc1dedc2777d3fbdec1337c
Kinrua <251955986@qq.com>
2023-07-21 19:22:54 +0800

    win壁纸拷贝

--- copy.py
+++ copy.py
@@ -4,18 +4,32 @@ Created on Sat Jul 21 00:35:33 2018
 
 @author: HCHO
 """
-#获取锁屏jpg图像
+
+# 获取锁屏jpg图像
 import os
 import shutil
+from PIL import Image
 os.popen('')
-#C:\Users\HCHO\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets
-oname="C:\\Users\\HCHO\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets"
-dname="C:\\Users\\HCHO\\Pictures\\Focusion"
-alllist=os.listdir(oname)
+UserName = "HCHO"
+opath = "C:\\Users\\" + UserName + "\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets"
+dpath_win = "C:\\Users\\" + UserName + "\\Pictures\\winWallpaper"
+dpath_android = "C:\\Users\\" + UserName + "\\Pictures\\androidWallpaper"
+all_list = os.listdir(opath)
 
-for item in alllist:
-        oldname=oname+'\\'+item
-        newname=dname+'\\'+item+".jpg"
-        fsize = os.path.getsize(oldname)/1024
-        if(fsize>200):
-            shutil.copyfile(oldname,newname)
+if not os.path.exists(dpath_win):
+    os.mkdir(dpath_win)
+if not os.path.exists(dpath_android):
+    os.mkdir(dpath_android)
+for item in all_list:
+    oldpath = opath + '\\' + item
+    img = Image.open(oldpath)
+    fsize = os.path.getsize(oldpath)/1024
+    if(fsize > 200):
+        if(img.size[0] > img.size[1]):
+            newpath = dpath_win + '\\' + item + '.jpg'
+        elif(img.size[0] < img.size[1]):
+            newpath = dpath_android + '\\' + item + '.jpg'
+        if not (os.path.isfile(newpath)):
+            print(newpath)
+            shutil.copyfile(oldpath, newpath)
+    img.close()
